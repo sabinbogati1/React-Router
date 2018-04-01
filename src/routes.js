b1dae62c71841473ae67da54482b88eb5ff0d590
sabinbogati1 <sabin.bogati@studentpartner.com>
2018-04-01 08:32:52 +0545

    87 passing control to redux form

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import {Route, IndexRoute} from "react-router";
 import PostsIndex from "./components/posts_index";
 import App from "./components/app";
+import PostsNew from "./components/posts_new";
  
 
 const Greeting =()=>{
@@ -10,6 +11,7 @@ const Greeting =()=>{
 export default(
     <Route path="/" component={App}>
             <IndexRoute component={PostsIndex} />
+            <Route path="posts/new" component={PostsNew} />
     </Route>
 );
  
